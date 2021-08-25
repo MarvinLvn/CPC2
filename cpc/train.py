653ac6e2ae108bcaff47eb8ef068d703ffc38e11
MarvinLvn <marvinlavechin@gmail.com>
2021-08-25 13:37:21 +0200

    Allow local_rank to be not read from checkpoint_args

--- cpc/train.py
+++ cpc/train.py
@@ -307,6 +307,7 @@ def main(argv):
             fl.loadArgs(args, locArgs,
                         forbiddenAttr={"nGPU", "pathCheckpoint",
                                        "debug", "restart", "world_size",
+                                       "global_rank", "local_rank"
                                        "n_nodes", "node_id", "n_gpu_per_node",
                                        "max_size_loaded", "nEpoch", "save_step"})
             args.load, loadOptimizer = [data], True
