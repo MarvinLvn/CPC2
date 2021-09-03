eba6a123aaaf061991761c90e60130ab794af84a
MarvinLvn <marvinlavechin@gmail.com>
2021-09-03 13:02:40 +0200

    Add followlinks=True to findAllSeqs

--- cpc/dataset.py
+++ cpc/dataset.py
@@ -938,7 +938,7 @@ def findAllSeqs(dirName,
     outIds = []
     idsTarget= {}
 
-    for root, dirs, filenames in tqdm.tqdm(os.walk(dirName)):
+    for root, dirs, filenames in tqdm.tqdm(os.walk(dirName, followlinks=True)):
         filtered_files = [f for f in filenames if f.endswith(extension)]
 
         if len(filtered_files) > 0:
